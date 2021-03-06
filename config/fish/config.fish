73b7eaa94073d41776d4cea32635cad21f3bc6ea
Corbin Bremmeyr <bremmeyr@mail.gvsu.edu>
2021-03-06 10:36:10 -0500

    initial commit

---
+++ config/fish/config.fish
@@ -0,0 +1,17 @@
+set fish_greeting
+fish_vi_key_bindings
+
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+
+alias n="nvim"
+
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gps="git push"
+alias gpl="git pull"
+alias gbr="git branch"
+alias gcl="git clone"
+
+set PATH $PATH $HOME/sh
