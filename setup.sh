4688933c2970eb096e2ffcc78f26136fa3aaced8
Corbin Bremmeyr <bremmeyr@mail.gvsu.edu>
2021-03-06 12:02:28 -0500

    Added fish install commands

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ cp -r sh/* $HOME/sh
 #
 # Instll fish
 #
-#cd install
-#tar -xf "fish-3.2.0.tar.xz"
-#cd "fish-3.2.0"
-#cd cmake .; make; su-exec make install
+cd install
+tar -xf "fish-3.2.0.tar.xz"
+cd "fish-3.2.0"
+cmake .; make; su-exec make install
 
